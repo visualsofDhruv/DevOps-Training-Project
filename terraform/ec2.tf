8448c2b1675676ec1c87d3ddf1322d7977e1ef1b
Dhruv Prajapati <dprajapati@splunk.com>
2021-05-16 11:01:53 +0530

    [ADDED] terraform Docs and Modified Terraform file

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,16 @@
 resource "aws_instance" "go_web_app" {
-  ami           = "ami-09e67e426f25ce0d7"
-  key_name = "my-server"
-  instance_type = "t2.micro"
-  security_groups= [ "gowebapp_security_group"]
-  tags= {
+  ami             = "ami-09e67e426f25ce0d7"
+  key_name        = "my-server"
+  instance_type   = "t2.micro"
+  security_groups = ["go_web_app_sg"]
+  tags = {
     Name = "go_web_app"
   }
 }
-
 #Create security group with firewall rules
-
-resource "aws_security_group" "gowebapp_security_group" {
-  name        = "gowebapp_security_group"
-  description = "security group for jenkins"
+resource "aws_security_group" "go_web_app_sg" {
+  name        = "go_web_app_sg"
+  description = "SG for go web app"
 
   ingress {
     from_port   = 9009
@@ -21,14 +19,28 @@ resource "aws_security_group" "gowebapp_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- # outbound from jenkis server
+  # outbound ports
   egress {
     from_port   = 0
     to_port     = 65535
@@ -36,16 +48,16 @@ resource "aws_security_group" "gowebapp_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
-    Name = "gowebapp_security_group"
+  tags = {
+    Name = "go_web_app_sg"
   }
 }
- 
+
 # Create Elastic IP address
 resource "aws_eip" "go_web_app" {
   vpc      = true
   instance = aws_instance.go_web_app.id
-tags= {
-    Name = "go_web_app_elastic_ip"
+  tags = {
+    Name = "go_web_app_eip"
   }
 }
